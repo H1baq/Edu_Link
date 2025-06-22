8b6bb01e01798fdb32fe87f80ec47285838d3320
hibby <hibaqku7@gmail.com>
2025-06-22 16:16:39 +0300

    fully functional frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Program from './Components/Programs'
+import About from './Components/About'
+import Contacts from './Components/Contacts'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Home/>
+      <Program/>
+      <About/>
+      <Contacts/>
+      <Footer/>
+
     </div>
   )
 }
